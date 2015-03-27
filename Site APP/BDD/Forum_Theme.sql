cf75a73a6feb3bda66379c118fdc4eb7c8dc241a
marine <marine.b_91@hotmail.fr>
2015-03-27 16:36:29 +0100

    bdd

---
+++ Site APP/BDD/Forum_Theme.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Ven 27 Mars 2015 à 16:26
+-- Version du serveur: 5.1.73
+-- Version de PHP: 5.3.3-7+squeeze19
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `marine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Forum_Theme`
+--
+
+CREATE TABLE IF NOT EXISTS `Forum_Theme` (
+  `id_forum` int(1) NOT NULL,
+  `id_theme` int(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
