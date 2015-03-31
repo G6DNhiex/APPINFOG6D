db3be69bda1be8dcc4833bdb753ffad8403b0910
marine <marine.b_91@hotmail.fr>
2015-03-31 11:37:27 +0200

    + nouveau index

---
+++ SiteAPP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+            <link rel="stylesheet" href="style.css" />
+            <link rel="stylesheet" href="seconnecter.css" />
+        <title>Se connecter</title>
+    </head>
+<?php
+include 'functions.php';
+
+top();
+?>
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+<?php
+bottom();
+?>
