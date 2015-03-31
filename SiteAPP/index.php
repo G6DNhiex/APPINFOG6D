2177fbfb557cf1ca40f9281503ff5643f92279b1
marine <marine.b_91@hotmail.fr>
2015-03-31 11:25:31 +0200

    index

---
+++ SiteAPP/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<link <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="style.css" />
+		<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<title>Accueil</title>
+	</head>
+
+
+
+<?php
+include 'functions.php';
+
+top();
+?>
+
+
+<?php
+bottom();
+?>
