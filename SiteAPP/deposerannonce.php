fd5c9ecaa4ee37c77ced8db89a1531a72726220a
florenceC <Florence@MacBook-Pro-de-Choquet.local>
2015-04-02 09:39:10 +0200

    déposer annonce

---
+++ SiteAPP/deposerannonce.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+            <link rel="stylesheet" href="style.css" />
+            <title>Déposer une annonce</title>
+        </head>
+<? php
+include 'functions.php'
+top();
+?>
+		
+<? php 
+bottom();
+?>
