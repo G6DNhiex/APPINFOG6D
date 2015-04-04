0e66012bda27aecab0899dfad14f8a85d95a763f
Knabaa <kevin.nabaa@gmail.com>
2015-04-04 17:08:37 +0200

    #pète un plomb sur wamp....

---
+++ SiteAPP/rechercheavancé.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<?php
+include 'functions.php';
+
+top();
+?>
+
+<form method="post" action="traitement.php">
+	<label> Mots-Clés </label>
+	 <input type="text" name="Mots-Clés" placeholder="Ex : Pomme"/><br>
+	 <label for="catégories"> Catégories/ Variétés</label>
+	 <select name="catégories" id="catégories">
+	 <option value="Choisissez une catégorie">Choisissez une catégorie</option></select>
+	 <select name="variétés">
+	 	<option value="Choisissez une variété"> choisissez une variété </option>
+	 </select><br>
+	 <label> Prix Min</label> <input type="number" name="Prix min" size="15"/>
+	 <label> Max </label> <input type="number" name="Prix max" size="2"/><br>
+	 <label> Quantité Min</label> <input type="number" name="Quantité min" size="15"/>
+	 <label> Max </label> <input type="number" name="Quantité max" size="2"/><br>
+	
+	 <label for="Echange"> Echange </label> <input type="checkbox" name="Echange" id="Echange"/> 
+</form>
+
+<?php
+bottom();
+?>
+
