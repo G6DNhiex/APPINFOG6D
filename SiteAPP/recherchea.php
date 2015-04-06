9054839ab76143be289a280f12572e7ab311d95e
Knabaa <kevin.nabaa@gmail.com>
2015-04-06 17:23:11 +0200

    Bit better

--- SiteAPP/recherchea.php
+++ SiteAPP/recherchea.php
@@ -10,25 +10,27 @@ include 'functions.php';
 
 top();
 ?>
+<h1 class="ravancee"> Recherche avancée </h1>
 
 <form method="post" action="traitement.php">
 	<p class="myformulaire">
-		<label> Mots-Clés </label>
-	 <input type="text" name="MotsCles" placeholder="Ex : Pomme" id="MotsClés"/><br>
-	 <label for="catégories"> Catégories/ Variétés</label>
-	 <select name="catégories" id="catégories">
+		<label class="Mcles"> Mots-Clés </label>
+	 <input type="text" name="MotsCles" placeholder="Ex : Pomme" class="MotsCles" /><br>
+	 <label for="catégories" class="cate"> Catégories/ Variétés</label>
+	 <select name="catégories" class="categories">
 	 <option value="Choisissez une catégorie">Choisissez une catégorie</option></select>
-	 <select name="variétés">
+	 <select name="variétés" class="varietes">
 	 	<option value="Choisissez une variété"> choisissez une variété </option>
 	 </select><br>
-	 <label id="prix"> Prix </label>
-	 <label id="Pmin"> Min </label> 
-	 <input type="number" name="Prix min" size="15"/>
-	 <label id)"Pmax"> Max </label> <input type="number" name="Prix max" size="2"/><br>
-	 <label id="quantité"> Quantité </label> <label id="Qmin"> Min </label><input type="number" name="Quantité min" size="15"/>
-	 <label id="Qmax"> Max </label> <input type="number" name="Quantité max" size="2"/><br>
+	 <label class="prix"> Prix </label>
+	 <label class="Pmin"> Min </label> 
+	 <input type="number" name="Prix min" size="15" class="Prixmin"/>
+	 <label class="Pmax"> Max </label> <input type="number" name="Prix max" size="2" class="Prixmax"/><br>
+	 <label class="quantite"> Quantité </label> <label class="Qmin"> Min </label><input type="number" name="Quantité min" size="15" class="Quantitiemin"/>
+	 <label class="Qmax"> Max </label> <input type="number" name="Quantité max" size="2" class="Quantitemax"/><br>
 	
-	 <label for="Echange"> Echange </label> <input type="checkbox" name="Echange" id="Echange"/> 
+	 <label for="Echange" class="echange"> Echange </label> <input type="checkbox" name="Echange" id="Echange" class="echange"/> 
+	 <input type="button" name="rechercher" class="rechercher" value="rechercher"/>
 	</p>
 </form>
 
